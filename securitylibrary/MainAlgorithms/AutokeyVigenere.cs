4a3b899f3276f2540a14dafaba61c9c292bc27a7
islamsaeed9854 <83390999+EslamSaeed9854@users.noreply.github.com>
2023-03-06 20:46:43 +0200

    finish_auto_repeatKey

--- securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -8,19 +8,66 @@ namespace SecurityLibrary
 {
     public class AutokeyVigenere : ICryptographicTechnique<string, string>
     {
+        char convert_Encrypt(char c1, char c2)
+        {
+            int x = c1 + (c2 - 97);
+            if (x > 122)
+                x = 97 + (x - 123);
+            return (char)x;
+        }
+        char convert_Decrypt(char c1, char c2)
+        {
+            int x = '-';
+            if (c2 >= c1)
+                x = (c2 - c1) + 97;
+            else
+                x = (123 - c1) + c2;
+            return (char)x;
+        }
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            string key = "";
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            for (int i =0;i < plainText.Length; i++)
+                key += convert_Decrypt(plainText[i], cipherText[i]);
+            string prefix = "", suffix = "";
+            for(int i = 0; i < plainText.Length; i++)
+            {
+                suffix = key[plainText.Length - i - 1] + suffix;
+                prefix += plainText[i];
+                if (suffix == prefix)
+                {
+                    key = key.Remove(plainText.Length - i - 1);
+                    return key;
+                }
+            }
+            return "";
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            string plan = "";
+            int size = cipherText.Length;
+            int key_pointer = 0;
+            while (key.Length != size)
+                key += convert_Decrypt(key[key_pointer], cipherText[key_pointer++]);
+            for (int i = 0; i < size; i++)
+                plan += convert_Decrypt(key[i], cipherText[i]);
+            return plan;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string cipher = "";
+            int size = plainText.Length;
+            int key_pointer = 0;
+            while (key.Length != plainText.Length)
+                key += plainText[key_pointer++ % size];
+            for (int i = 0; i < size; i++)
+                cipher += convert_Encrypt(plainText[i], key[i]);
+            return cipher;
         }
     }
 }
--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -8,19 +8,70 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
+        char convert_Encrypt(char c1, char c2)
+        {
+            int x = c1 + (c2 - 97);
+            if (x > 122)
+                x = 97 + (x - 123);
+            return (char)x;
+        }
+        char convert_Decrypt(char c1, char c2)
+        {
+            int x = '-';
+            if (c2 >= c1)
+                x = (c2 - c1)+97;
+            else
+                x = (123 - c1) + c2;
+            return (char)x;
+        }
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            string key = "";
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            for (int i = 0; i < plainText.Length; i++)
+                key += convert_Decrypt(plainText[i], cipherText[i]);
+            string prefix = "";
+            for(int i = 0;i < key.Length; i++)
+            {
+                prefix+=key[i];
+                int idx = 0;
+                for (int j = i+1; j < key.Length; j++)
+                {
+                    if (prefix[idx] != key[j]) break;
+                    if(j==key.Length-1)
+                        return prefix;
+                    idx++;
+                    if (idx == prefix.Length)
+                        idx = 0;
+                }
+            }
+            return key;
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            string plan = "";
+            int size = cipherText.Length;
+            int key_pointer = 0;
+            while (key.Length != size)
+                key += key[key_pointer++ % size];
+            for (int i = 0; i < size; i++)
+                plan += convert_Decrypt(key[i],cipherText[i]);
+            return plan;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string cipher = "";
+            int size = plainText.Length;
+            int key_pointer = 0;
+            while (key.Length != plainText.Length)
+                key += key[key_pointer++%size];
+            for (int i = 0; i < size; i++)
+                cipher += convert_Encrypt(plainText[i], key[i]);
+            return cipher;
         }
     }
 }
