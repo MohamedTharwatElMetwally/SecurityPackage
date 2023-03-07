5c0ea94616bbe208e3f91360c51cbd1d4a014cc1
MAbdelhamid2001 <MAbdelhamid2001@Github.com>
2023-03-07 07:30:20 +0200

    ceaser finished besides mono ,only 3 testcases on monoalphabetic left

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -10,17 +10,56 @@ namespace SecurityLibrary
     {
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            String c = "";
+            foreach (char l in plainText.ToLower())
+            {
+                int p_idx = alpha.IndexOf(char.ToUpper(l));
+
+                int c_idx = (p_idx + key) % alpha.Length;
+
+
+                c += (char)alpha[c_idx];
+            }
+
+
+
+            return c;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int p_idx = 0;
+            String p = "";
+            foreach (char l in cipherText)
+            {
+
+                int c_idx = alpha.IndexOf(char.ToUpper(l));
+
+                int new_k = alpha.Length - key;
+
+                p_idx = (c_idx + new_k) % alpha.Length;
+
+                p += (char)alpha[p_idx];
+            }
+            return p.ToLower();
         }
 
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int k = 0;
+
+            int p_idx = alpha.IndexOf(char.ToUpper(plainText[0]));
+            int c_idx = alpha.IndexOf(char.ToUpper(cipherText[0]));
+            k = c_idx - p_idx;
+            if (k > 0)
+                return k;
+            else if (k < 0) return k + alpha.Length;
+            else return 0;
         }
     }
 }
+
