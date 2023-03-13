689a330a00c44e5348f1ea301f5921cac5934540
Amr Wael <amr.wael.dev@gmail.com>
2023-03-13 04:37:20 +0200

    Add Columnar & RailFence Algorithms

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,113 @@ namespace SecurityLibrary
 {
     public class Columnar : ICryptographicTechnique<string, List<int>>
     {
+        // REFERENCE: https://www.chadgolden.com/blog/finding-all-the-permutations-of-an-array-in-c-sharp
+        static IList<IList<int>> Permute(int[] nums) {
+            var list = new List<IList<int>>();
+            return DoPermute(nums, 0, nums.Length - 1, list);
+        }
+
+        static IList<IList<int>> DoPermute(int[] nums, int start, int end, IList<IList<int>> list) {
+            if (start == end) {
+                // We have one of our possible n! solutions,
+                // add it to the list.
+                list.Add(new List<int>(nums));
+            } else {
+                for (var i = start; i <= end; i++) {
+                    Swap(ref nums[start], ref nums[i]);
+                    DoPermute(nums, start + 1, end, list);
+                    Swap(ref nums[start], ref nums[i]);
+                }
+            }
+
+            return list;
+        }
+
+        static void Swap(ref int a, ref int b) {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+        // -------------------------------------------- END ---------------------------------------------
+
+        // NEWLY ADDED (NOT INCLUDED IN BASIC SECURITY PACKAGE)
+        private IList<IList<int>> GetLists(List<int> key) {
+            return Permute(key.ToArray());
+        }
+        // ----------------------- END ------------------------
+
         public List<int> Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToLower(); cipherText = cipherText.ToLower();
+
+            List<int> key = new List<int>();
+
+            for (int temp = 1; temp <= plainText.Length; temp++) {
+                key.Add(temp);
+
+                IList<IList<int>> lists = GetLists(key);
+
+                foreach (var list in lists) {
+                    Columnar columnar = new Columnar();
+                    if (columnar.Encrypt(plainText, (List<int>)list) == cipherText || columnar.Decrypt(cipherText, (List<int>)list) == plainText) {
+                        return (List<int>)list;
+                    }
+                }
+            }
+            
+            throw new Exception();
         }
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            // Create the table of desired height
+            int nRows = (int)Math.Ceiling((double)cipherText.Length / key.Count);
+            List<List<char>> table = new List<List<char>>();
+            for (int i = 0; i < key.Count; i++)
+                table.Add(new List<char>());
+
+            // Fill the table column-wise with-respect-to key indices
+            // Place '-' for empty places
+            int ctIndex = 0;
+            for (int i = 0; i < table.Count; i++)
+                for (int j = 0; j < nRows; j++, ctIndex++)
+                    try { table[key.IndexOf(i + 1)].Add(cipherText[ctIndex]); }
+                    catch (IndexOutOfRangeException) { table[key.IndexOf(i + 1)].Add('-'); }
+
+            // Read the table row-wise
+            string plain = "";
+            for (int j = 0; j < nRows; j++)
+                for (int i = 0; i < table.Count; i++)
+                    if (table[i][j] != '-')
+                        plain += table[i][j];
+
+            return plain;
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            // Create the table of desired height
+            int nRows = (int)Math.Ceiling((double)plainText.Length / key.Count);
+            List<List<char>> table = new List<List<char>>();
+            for (int i = 0; i < nRows; i++)
+                table.Add(new List<char>());
+
+            // Fill the table row-wise
+            // Place '-' for empty places
+            int ptIndex = 0;
+            for (int i = 0; i < table.Count; i++)
+                for (int j = 0; j < key.Count; j++, ptIndex++)
+                    try { table[i].Add(plainText[ptIndex]); }
+                    catch (IndexOutOfRangeException) { table[i].Add('-'); }
+
+            // Read the table column-wise with-respect-to key indices
+            string cipher = "";
+            for (int i = 0; i < key.Count; i++)
+                for (int j = 0; j < table.Count; j++)
+                    if (table[j][key.IndexOf(i + 1)] != '-')
+                        cipher += table[j][key.IndexOf(i + 1)];
+
+            return cipher;
         }
     }
 }
--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,74 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+
+            int key = 0;
+            RailFence rf = new RailFence();
+            while (key <= plainText.Length) {
+                if (rf.Encrypt(plainText, key) == cipherText || rf.Decrypt(cipherText, key) == plainText)
+                    return key;
+
+                key++;
+            }
+
+            return key;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+
+            // Create the table of desired width
+            List<List<char>> table = new List<List<char>>();
+            for (int i = 0; i < key; i++)
+                table.Add(new List<char>());
+
+            // Fill the table row-wise
+            // Place '-' for empty places
+            int lettersInRow = (int)Math.Ceiling((double)cipherText.Length / key);
+            int ctIndex = 0;
+            for (int i = 0; i < table.Count; i++)
+                for (int j = 0; j < lettersInRow; j++, ctIndex++)
+                    try { table[i].Add(cipherText[ctIndex]); }
+                    catch (IndexOutOfRangeException) { table[i].Add('-'); }
+
+            // Read the table column-wise
+            string plain = "";
+            for (int j = 0; j < lettersInRow; j++)
+                for (int i = 0; i < table.Count; i++)
+                    if (table[i][j] != '-')
+                        plain += table[i][j];
+
+
+            return plain;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            // Create the table of desired width
+            List<List<char>> table = new List<List<char>>();
+            for (int i = 0; i < key; i++)
+                table.Add(new List<char>());
+
+            // Fill the table column-wise
+            // Place '-' for empty places
+            int lettersInRow = (int)Math.Ceiling((double)plainText.Length / key);
+            int ptIndex = 0;
+            for (int j = 0; j < lettersInRow; j++)
+                for (int i = 0; i < table.Count; i++, ptIndex++)
+                    try { table[i].Add(plainText[ptIndex]); }
+                    catch (IndexOutOfRangeException) { table[i].Add('-'); }
+
+            // Read the table row-wise
+            string cipher = "";
+            for (int i = 0; i < table.Count; i++)
+                for (int j = 0; j < lettersInRow; j++)
+                    if (table[i][j] != '-')
+                        cipher += table[i][j];
+
+            return cipher;
         }
     }
 }
