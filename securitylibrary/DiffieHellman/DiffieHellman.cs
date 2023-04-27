5ff9b77638fea68edc68a6757956dbc84dd0dc46
islamsaeed9854 <83390999+islamsaeed9854@users.noreply.github.com>
2023-04-27 13:15:57 +0200

    applying diff&hellman, ElGammal Algorithm

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace SecurityLibrary.DiffieHellman
 {
     public class DiffieHellman 
     {
+        static int Fast_P(int bas, int exp, int mod)
+        {
+            int ans = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    ans = (ans * bas) % mod;
+                exp /= 2;
+                bas = (bas * bas) % mod;
+            }
+
+            return ans;
+        }
+
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            int g = alpha;
+            int prime = q;
+            int private_1 = xa;
+            int private_2 = xb;
+
+            int A = Fast_P(g, private_1, prime);
+            int B = Fast_P(g, private_2, prime);
+
+            int secret_A = Fast_P(B, private_1, prime);
+
+            List<int> result = new List<int>();
+
+            result.Add(secret_A);
+            result.Add(secret_A);
+            return result;
+
         }
     }
 }
--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -16,14 +16,43 @@ namespace SecurityLibrary.ElGamal
         /// <param name="y"></param>
         /// <param name="k"></param>
         /// <returns>list[0] = C1, List[1] = C2</returns>
-        public List<long> Encrypt(int q, int alpha, int y, int k, int m)
+        /// 
+        static long Fast_P(long bas, long exp, long mod)
         {
-            throw new NotImplementedException();
+            long ans = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    ans = (ans * bas) % mod;
+                exp /= 2;
+                bas = (bas * bas) % mod;
+            }
+            return ans;
+        }
+
+        static long mod_inverse(long x, long mod)
+        {
+            return Fast_P(x, mod - 2,mod);
+        }
+        
+
+        public List<long> Encrypt(int q, int alpha, int y, int k, int m)   // k => random number < q
+        {
+            
+            long c1 = Fast_P(alpha, k, q);
+            long  c2 = (m * Fast_P(y, k, q)) % q;
+            List<long> ans = new List<long>();
+            ans.Add(c1);
+            ans.Add(c2);
+            Console.WriteLine(c1 + " " + c2);
+            return ans;
 
         }
         public int Decrypt(int c1, int c2, int x, int q)
         {
-            throw new NotImplementedException();
+            long s = Fast_P(c1, x, q);
+            long m = (c2 * mod_inverse(s, q)) % q;
+            return (int)m;
 
         }
     }
