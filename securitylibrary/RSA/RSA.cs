51681420b7d4129a5cfe28549a5c5efb44c9c0b2
MohamedTharwatElMetwally <mt2910201@gmail.com>
2023-04-27 15:17:04 +0200

    RSA has been developed and everything works properly

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SecurityLibrary.AES;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,40 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            return (int)ModularBinaryExponentiation(M, e, p * q);   // C = M^e % n, where n = p * q
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            return (int)ModularBinaryExponentiation(C, ModInverse2(e, (p - 1) * (q - 1)), p * q);    // M = C^d % n, where n = p * q, d = e^-1 % Alpha(n) and Alpha(n) = (p-1) * (q-1)  
+        }
+
+        public long ModulusMul(long x, long y, long m)
+        {
+            return ((x % m) * (y % m)) % m;
+        }
+
+        public long ModularBinaryExponentiation(long num, long exp, long mod)
+        {
+            long result = 1;
+            while (exp > 0)
+            {
+                if (exp % 2 != 0) result = ModulusMul(result, num, mod);
+                exp /= 2;
+                num = ModulusMul(num, num, mod);
+            }
+            return result;
+        }
+
+
+        public long ModInverse2(int A, int M)
+        {
+            ExtendedEuclid extendedEuclid = new ExtendedEuclid();
+            return extendedEuclid.GetMultiplicativeInverse(A, M);
         }
     }
 }
